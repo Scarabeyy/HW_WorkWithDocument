600faf454d1e6c277636fffa6c810567a9d651e0
Gleb <Obsidian63@yandex.ru>
2019-03-17 20:57:12 +0400

    some new

---
+++ Module1.js
@@ -0,0 +1,12 @@
+var Module = {};
+
+Module.changeAll = function () {
+
+    var arr=document.body.childNodes;
+
+    for(var i = 0; i < arr.length;i++){
+        if(arr[i].nodeType == 1){
+            arr[i].innerHTML = "PARAGRAPH";
+        }
+    }
+}
